e3f90b5b51a2d04a25e0a326980092ae85c2d773
Ehren Castillo <ehrencastillo@gmail.com>
2024-06-15 16:45:12 +0800

    refactor(navbar): add `NavButton` component for navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,16 @@
 <script>
+    import NavButton from '$lib/components/NavButton.svelte';
     import '../app.css';
 </script>
 
-<nav class="navbar bg-primary justify-center gap-2 sticky top-0">
-    <a href="#top" class="btn btn-primary hover:bg-neutral-700 text-lg">Top</a>
-    <a href="#top" class="btn btn-primary hover:bg-neutral-700 text-lg">Overview</a>
-    <a href="#top" class="btn btn-primary hover:bg-neutral-700 text-lg">Background</a>
-    <a href="#top" class="btn btn-primary hover:bg-neutral-700 text-lg">Collection</a>
-    <a href="#top" class="btn btn-primary hover:bg-neutral-700 text-lg">Methodology</a>
-    <a href="#top" class="btn btn-primary hover:bg-neutral-700 text-lg">Results</a>
-    <a href="#top" class="btn btn-primary hover:bg-neutral-700 text-lg">Team</a>
+<nav class="navbar min-h-12 lg:min-h-16 bg-primary justify-center gap-2 sticky top-0">
+    <NavButton href="#top">Top</NavButton>
+    <NavButton href="#top">Overview</NavButton>
+    <NavButton href="#top">Background</NavButton>
+    <NavButton href="#top">Collection</NavButton>
+    <NavButton href="#top">Methodology</NavButton>
+    <NavButton href="#top">Results</NavButton>
+    <NavButton href="#top">Team</NavButton>
 </nav>
 
 <slot />
