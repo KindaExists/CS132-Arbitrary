64864ff773b42dc7e3dc98fb22b4cfbd22d9bce2
Ehren Castillo <ehrencastillo@gmail.com>
2024-06-16 02:48:08 +0800

    feat(background): add background section

---
+++ src/lib/components/BackgroundCard.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    export let header: string;
+</script>
+
+<div class="flex flex-row gap-4">
+    <div class="flex flex-col justify-center border-r-2 border-r-neutral-300 w-52 p-4">
+        <h3 class="text-2xl font-bold">{header}</h3>
+    </div>
+    <div class="flex flex-col justify-center w-full my-4">
+        <div class="bg-white border-l-[1em] border-neutral-700 shadow-[0.5em_0.5em_0_#e8e8e8] flex flex-col justify-center w-full px-8 py-2">
+            <p class="text-2xl"><slot /></p>
+        </div>
+    </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,14 +4,15 @@
     import smsIcon from '$lib/images/sms-icon.png';
     import gavelIcon from '$lib/images/gavel.png';
     import chipIcon from '$lib/images/tech-chip.png';
+	import BackgroundCard from '$lib/components/BackgroundCard.svelte';
 </script>
 
-<article class="bg-white flex flex-col lg:flex-row items-center px-8 lg:px-56 py-32 gap-4 lg:gap-12 shadow-md">
+<article id="top" class="bg-white flex flex-col lg:flex-row items-center px-8 lg:px-56 py-32 gap-4 lg:gap-12 shadow-md">
     <div class="flex flex-col justify-center gap-6">
-        <h1 class="text-6xl">
+        <h1 class="text-6xl leading-tight">
             Hi. We are <strong class="font-bold">Arbitrary</strong>. <br/>
             This is our project, <br/>
-            <strong class=font-bold>SIM Spam Slam</strong>
+            <strong class="font-bold hover:text-accent transition-colors">SIM Spam Slam</strong>
         </h1>
         <p class="prose text-2xl">
             Like the popular canned food brand it is named after, spam texts can come in all
@@ -22,7 +23,7 @@
     <img src={wordcloud} alt="word cloud" class="aspect-[1446/2139] w-full lg:w-80"/>
 </article>
 
-<article class="flex flex-col gap-12 px-8 lg:px-56 py-32">
+<article id="overview" class="flex flex-col gap-12 px-8 lg:px-56 py-32 shadow-md">
     <section class="flex flex-col gap-4 items-center">
         <h2 class="text-3xl lg:text-4xl font-bold">Overview</h2>
         <h3 class="text-2xl font-bold">What is this project for?</h3>
@@ -73,3 +74,36 @@
     </section>
 </article>
 
+<article id="background" class="flex flex-col gap-12 px-8 lg:px-56 py-32 shadow-md">
+    <section class="flex flex-col items-center gap-4">
+        <h2 class="text-3xl lg:text-4xl font-bold">Background</h2>
+        <h3 class="text-2xl text-center font-bold">
+            Despite the country's best efforts, Spam texts remain a prevalent and persistent
+            form of cybercriminal activity, with millions of messages being detected as of 2024.
+        </h3>
+        <div class="prose max-w-full">
+            <p class="text-xl lg:text-2xl text-center m-0">
+                Telecommunication companies such as Globe, PLDT and Smart have taken action against
+                5.5 million spam messages in the past year. However, even with this preventive action,
+                the Cybercrime Investigation and Coordinating Center (CICC) noted that the losses of
+                Filipino mobile phone users to spam texts have reached <em>millions of dollars</em>.
+                Moreover, some instances of spam messages have also been found in instant messaging apps,
+                which are not covered by the Sim Registration Act.
+            </p>
+            <p class="text-lg text-center mt-1">
+                References:
+                <a target="_blank" href="https://business.inquirer.net/427513/spam-still-hounding-ph-over-5-5m-messages-blocked" class="link link-accent">[1]</a>
+                <a target="_blank" href="https://www.gmanetwork.com/news/topstories/nation/846936/filipinos-lost-millions-of-dollars-due-to-text-scams-cicc/story/" class="link link-accent">[2]</a>
+                <a target="_blank" href="https://www.philstar.com/headlines/2023/09/02/2293211/text-scams-have-shifted-online-apps-globe-warns" class="link link-accent">[3]</a>
+            </p>
+        </div>
+    </section>
+    <section class="flex flex-col items-center gap-4">
+        <h3 class="text-2xl lg:text-3xl font-bold">What do we aim to do?</h3>
+        <BackgroundCard header="Main Question">What could be the trends and patterns of spam messages in the Philippines?</BackgroundCard>
+        <BackgroundCard header="Secondary Question">Is there a difference between Spam messages before and after the deadline of the Sim Card Registration Act?</BackgroundCard>
+        <BackgroundCard header="Null Hypothesis">The SIM card registration act has <strong class="font-bold">no</strong> effect on the content of spam messages.</BackgroundCard>
+        <BackgroundCard header="Alternative Hypothesis">The SIM card registration act has an effect on the content of spam messages.</BackgroundCard>
+        <BackgroundCard header="Action Plan">Utilize data science tools and Natural Language Processing (NLP) to identify the patterns and trends of Spam text content.</BackgroundCard>
+    </section>
+</article>
