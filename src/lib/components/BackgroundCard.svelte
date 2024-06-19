1ca8a6cce5bdd561b0fbdd0abd050348700dd617
Ehren Castillo <ehrencastillo@gmail.com>
2024-06-19 12:40:09 +0800

    fix(BackgroundCard): fix card alignment for small screens

--- src/lib/components/BackgroundCard.svelte
+++ src/lib/components/BackgroundCard.svelte
@@ -2,13 +2,13 @@
     export let header: string;
 </script>
 
-<div class="flex flex-row gap-4">
-    <div class="flex flex-col justify-center border-r-2 border-r-neutral-300 w-52 p-4">
-        <h3 class="text-2xl font-bold">{header}</h3>
+<div class="flex flex-col lg:flex-row lg:gap-4 justify-stretch items-stretch w-full">
+    <div class="flex flex-col justify-center items-center lg:items-start border-0 lg:border-r-2 border-r-neutral-300 lg:w-60 lg:p-4">
+        <h3 class="text-xl lg:text-2xl font-bold">{header}</h3>
     </div>
     <div class="flex flex-col justify-center w-full my-4">
         <div class="bg-white border-l-[1em] border-neutral-700 shadow-[0.5em_0.5em_0_#e8e8e8] flex flex-col justify-center w-full px-8 py-2">
-            <p class="text-2xl"><slot /></p>
+            <p class="prose text-xl lg:text-2xl"><slot /></p>
         </div>
     </div>
 </div>
